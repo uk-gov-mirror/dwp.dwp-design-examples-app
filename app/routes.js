c45401138b889cacf5082e65f12e4d6407e11de9
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-07 16:28:26 +0100

    Updated functions and filenames

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express')
 const router = express.Router()
 
@@ -9,13 +10,35 @@ router.get('/', function (req, res) {
 router.get('/example/:example', (req, res) => {
   const example = req.params.example
   const content = require(`./views/design-examples/${example}/_page-content`)
-  res.render('example', {example, content})
+  const code = getCodeFiles(example)
+  res.render('example', {example, content, code})
 })
 
 router.get('/live-demo/:example', (req, res) => {
   const example = req.params.example
   const content = require(`./views/design-examples/${example}/_page-content`)
-  res.render('live-demo', {example, content})
+  const code = getCodeFiles(example)
+  res.render('live-demo', {example, content, code})
 })
+
+function getCodeFiles (example) {
+  const code = {}
+  if (fs.existsSync(__dirname + `/views/design-examples/${example}/index.html`)) {
+    code.HTML = fs.readFileSync(__dirname + `/views/design-examples/${example}/index.html`)
+  }
+  if (fs.existsSync(__dirname + `/views/design-examples/${example}/scripts.js`)) {
+    code.JS = fs.readFileSync(__dirname + `/views/design-examples/${example}/scripts.js`)
+  }
+  if (fs.existsSync(__dirname + `/views/design-examples/${example}/styles.css`)) {
+    code.CSS = fs.readFileSync(__dirname + `/views/design-examples/${example}/styles.css`)
+  }
+  if (fs.existsSync(__dirname + `/views/design-examples/${example}/_styles.scss`)) {
+    code.SASS = fs.readFileSync(__dirname + `/views/design-examples/${example}/_styles.scss`)
+  }
+  if (fs.existsSync(__dirname + `/views/design-examples/${example}/sketch.zip`)) {
+    code.SKETCH = `/views/design-examples/${example}/sketch.zip`
+  }
+  return code;
+}
 
 module.exports = router
