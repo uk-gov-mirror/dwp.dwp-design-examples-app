0d25215efa5c1e7c780604fdf973d9bcd6c9f9fb
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-07 14:50:51 +0100

    Made content dynamic

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,16 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-// Add your routes here - above the module.exports line
+router.get('/example/:example', (req, res) => {
+  const example = req.params.example
+  const content = require(`./views/design-examples/${example}/_page-content`)
+  res.render('example', {example, content})
+})
+
+router.get('/live-demo/:example', (req, res) => {
+  const example = req.params.example
+  const content = require(`./views/design-examples/${example}/_page-content`)
+  res.render('live-demo', {example, content})
+})
 
 module.exports = router
