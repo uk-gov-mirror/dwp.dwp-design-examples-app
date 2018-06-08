361aa15bd6a8c2ead81bba9b96b8138e5c1bba28
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-08 14:20:00 +0100

    Added timeline as experimental

---
+++ app/views/design-examples/timeline/_page-content.js
@@ -0,0 +1,36 @@
+module.exports = {
+  h1: 'Timeline',
+  // status: 1 = experimental, 2 = tried and tested, 3 = recommended
+  status: 1,
+  description: `
+    <p>
+      A way to display a list of actions that have been done.
+    </p>
+  `,
+  whenToUse: `
+    <p>
+      To follow. 
+    </p>
+  `,
+  whenNotToUse: `
+    <p>
+      To follow
+    </p>
+  `,
+  research: `
+    <ul class="list list-bullet">
+      <li>User type: Agents</li>
+      <li>Accessibility: Tested on JAWS, Dragon, Zoomtext and Read&Write</li>
+    </ul>
+  `,
+  servicesUsing: `
+    <ul class="list list-bullet">
+      <li>Manage Bereavement Support Payment</li>
+      <li>Access to work</li>
+    </ul>
+  `,
+  discussion: {
+    linkText: 'Discuss the timeline on GitHub',
+    linkURL: 'https://github.com/dwp/design-examples/issues/3'
+  }
+}
